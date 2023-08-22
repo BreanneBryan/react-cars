903e5b3f3800f6d179de612256071826466ed812
Breanne Bryan <126629243+BreanneBryan@users.noreply.github.com>
2023-08-22 18:25:11 -0500

    Add files via upload

---
+++ src/config/auth0.config.ts
@@ -0,0 +1,4 @@
+export const auth0Config = {
+    domain: "dev-a8gib3uaj1e66l1p.us.auth0.com",
+    clientId: "aleV5m2HFEsWrbq3Vw8cZtPErNy1Inrr",
+  };
---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="text-center p-5"><h1>Here is a joke for your entertainment.</h1>
+    <p>What do you get when dinosaurs crash their cars?</p>
+    <p>Tyrannosaurus wrecks!</p>
+    <p>Did ya get it?</p>
+
+    </div>
+  )
+}
+
+export default About
